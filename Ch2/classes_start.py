f576dfeeb9184f37ee89ac227a957c8f805d9c46
hjeisen <howard@rocknrollrobots25.com>
2020-06-30 23:42:09 +0000

    finished Ch2

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -1,9 +1,28 @@
 #
 # Example file for working with classes
 #
+class myClass():
+    def method1(self):
+        print("myClass method1")
 
+    def method2(self, someString):
+        print("My class method2 "+someString)
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someString):
+        print("anotherclass method2 ")
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("THis is a string")
+    c22 = anotherClass()
+    c22.method1()
+    c22.method2("string test")
 
   
 if __name__ == "__main__":
