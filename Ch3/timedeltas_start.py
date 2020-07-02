2088eebb87c131da42375935e8601d83cab610e9
hjeisen <howard@rocknrollrobots25.com>
2020-07-02 20:29:13 +0000

    ch4 complete

--- Ch3/timedeltas_start.py
+++ Ch3/timedeltas_start.py
@@ -5,25 +5,35 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 
 # construct a basic timedelta and print it
+# print(timedelta(days=365, hours = 5, minutes=1))
 
+# # print today's date
+now = datetime.now()
+print(now)
 
-# print today's date
-
-
-# print today's date one year from now
-
+# # print today's date one year from now
+# print("One year from now it will be: " + str (now + timedelta(days=365)))
 
 # create a timedelta that uses more than one argument
-
+print("In 2 days and 3 weeks it will be: " + str(now+timedelta(days=2, weeks=3)))
 
 # calculate the date 1 week ago, formatted as a string
-
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print("One week ago it was "+s)
 
 ### How many days until April Fools' Day?
 
+today = date.today()
+afd = date(today.year, 4, 1 )
+if afd < today:
+    print ("April fools day went by %d days ago" % (today-afd).days)
+    afd = afd.replace(year=today.year+1)
+print("Next April fools is %d days from now" % (afd-today).days)
 
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
